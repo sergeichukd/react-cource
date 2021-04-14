35c9214c5c76c53529b33475d94e1f34aae5168a
Dmitrii Sergeichuk <sergeychukdmitry@gmail.com>
2021-04-14 21:43:43 +0300

    Refactor

--- lesson-5-dz/src/components/Footer.jsx
+++ lesson-5-dz/src/components/Footer.jsx
@@ -3,8 +3,6 @@ import FooterColumn from './FooterColumn.jsx'
 
 export default class Footer extends React.Component {
   render() {
-    console.log('Footer: data')
-    console.dir(this.props.data)
     const columns = Object.keys(this.props.data).map((key, index) => {
       return <FooterColumn data={this.props.data[key]} key={key}/>
     })
